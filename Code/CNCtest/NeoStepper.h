dc5a38de64162e6f5b21026ea7d5e8e193685357
Skkeye <skkeye@gmail.com>
2023-02-28 16:18:22 -0500

    quick update

--- Code/CNCtest/NeoStepper.h
+++ Code/CNCtest/NeoStepper.h
@@ -16,7 +16,6 @@ private:
   // Identification
   int _pinA0, _pinA1, _pinB0, _pinB1;
   int axis;  // X: 0; Y: 1; Z: 2;
-
   // State
   int _currentState = 0;
   int _states[8] = { 0b0001, 0b0101, 0b0100, 0b0110, 0b0010, 0b1010, 0b1000, 0b1001 };
